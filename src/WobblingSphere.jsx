b0f2a97df954979dc212c18f9146f260081cb1c4
Alina <alina.andrevna3993@gmail.com>
2023-08-25 13:29:04 +0300

    added mouse

--- src/WobblingSphere.jsx
+++ src/WobblingSphere.jsx
@@ -13,6 +13,66 @@ const AnimatedMaterial = a(MeshDistortMaterial);
 
 const WobblingSphere = () => {
   const sphere = useRef();
+  const matRef = useRef();
+  const mouse = {
+    x: 0,
+    y: 0,
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", (event) => {
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    });
+  }, []);
+
+  const {
+    color,
+    envMapIntensity,
+    clearcoat,
+    clearcoatRoughness,
+    metalness,
+    distort,
+    speed,
+  } = useControls("FlowerShader", {
+    color: "#1EFFA5",
+    envMapIntensity: {
+      value: 0.5,
+      step: 0.01,
+      min: 0,
+      max: 2,
+    },
+    clearcoat: {
+      value: 0.2,
+      step: 0.01,
+      min: 0,
+      max: 2,
+    },
+    clearcoatRoughness: {
+      value: 0.4,
+      step: 0.01,
+      min: 0,
+      max: 2,
+    },
+    metalness: {
+      value: 0.1,
+      step: 0.01,
+      min: 0,
+      max: 2,
+    },
+    distort: {
+      value: 1.2,
+      step: 0.01,
+      min: 0,
+      max: 5,
+    },
+    speed: {
+      value: 0.7,
+      step: 0.01,
+      min: 0,
+      max: 20,
+    },
+  });
 
   useFrame((state) => {
     if (sphere.current) {
@@ -27,48 +87,14 @@ const WobblingSphere = () => {
         0.2
       );
     }
-  });
 
-  const { color, envMapIntensity, clearcoat, clearcoatRoughness, metalness, distort, speed } =
-    useControls("FlowerShader", {
-      color: "#1EFFA5",
-      envMapIntensity: {
-        value: 0.5,
-        step: 0.01,
-        min: 0,
-        max: 2,
-      },
-      clearcoat: {
-        value: 0.2,
-        step: 0.01,
-        min: 0,
-        max: 2,
-      },
-      clearcoatRoughness: {
-        value: 0.4,
-        step: 0.01,
-        min: 0,
-        max: 2,
-      },
-      metalness: {
-        value: 0.1,
-        step: 0.01,
-        min: 0,
-        max: 2,
-      },
-      distort: {
-        value: 0.9,
-        step: 0.01,
-        min: 0,
-        max: 5,
-      },
-      speed: {
-        value: 0.7,
-        step: 0.01,
-        min: 0,
-        max: 20,
-      },
-    });
+    if (mouse.x && mouse.y && matRef) {
+      matRef.current.distort = distort - Math.abs(mouse.x * 0.7);
+
+      sphere.current.rotation.x = 0 - mouse.y * 0.3;
+      sphere.current.rotation.y = 0 + mouse.x * 0.5;
+    }
+  });
 
   return (
     <>
@@ -76,6 +102,7 @@ const WobblingSphere = () => {
         <a.mesh ref={sphere} scale={0.4}>
           <sphereBufferGeometry args={[1, 64, 64]} />
           <AnimatedMaterial
+            ref={matRef}
             color={color}
             envMapIntensity={envMapIntensity}
             clearcoat={clearcoat}
